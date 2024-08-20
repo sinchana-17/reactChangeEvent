6122a3202597de98010e180d915ae0d43402a41d
sinchana-17 <sinchanapshetti17@gmail.com>
2024-08-20 10:49:38 +0530

    Initial commit

---
+++ src/Form.jsx
@@ -0,0 +1,31 @@
+import {useState} from 'react'
+ //import { ToastContainer, toast } from 'react-toastify';
+ // import 'react-toastify/dist/ReactToastify.css';
+const Form=()=>{
+    const [userName,setUserName]=useState("")
+    const [newUserDetails,setNewUserDetails]=useState()
+   // const notify = () => toast("Submitted successfully!");
+
+    const getUserName=(event)=>{
+        setUserName(event.target.value)
+        }
+
+    const userDetails=()=>{
+        setNewUserDetails(userName)
+     //   notify()
+        }
+
+    return(
+
+            <section className="FormSection">
+                <h3>{newUserDetails}</h3>
+              {/* <ToastContainer/> */}
+            <div className="Input">
+                <input type="text" placeholder="Enter your name" onChange={getUserName}/>
+                <br/>
+                <button onClick={userDetails}>Submit</button>
+                </div>
+                </section>
+        )
+    }
+export default Form;
