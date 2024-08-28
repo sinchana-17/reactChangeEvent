77bd999f1332cf3a32703fe9e7e995a2af10a34d
sinchana-17 <sinchanapshetti17@gmail.com>
2024-08-28 10:59:13 +0530

    Initial Commit

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,10 +1,11 @@
 import {useState} from 'react'
- //import { ToastContainer, toast } from 'react-toastify';
- // import 'react-toastify/dist/ReactToastify.css';
+
+ import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 const Form=()=>{
     const [userName,setUserName]=useState("")
     const [newUserDetails,setNewUserDetails]=useState()
-   // const notify = () => toast("Submitted successfully!");
+    const notify = () => toast("Successfully completed!");
 
     const getUserName=(event)=>{
         setUserName(event.target.value)
@@ -12,14 +13,14 @@ const Form=()=>{
 
     const userDetails=()=>{
         setNewUserDetails(userName)
-     //   notify()
+        notify()
         }
 
     return(
 
             <section className="FormSection">
                 <h3>{newUserDetails}</h3>
-              {/* <ToastContainer/> */}
+                <ToastContainer/>
             <div className="Input">
                 <input type="text" placeholder="Enter your name" onChange={getUserName}/>
                 <br/>
